98ec7aa61c1b93579d54c7b415d29f291591d25d
Sonnydread <alvaradoguille590@gmail.com>
2023-08-29 16:22:55 -0700

    the-float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""takes a float n return the floor of the float"""
+import math
+
+
+def floor(n: float) -> int:
+    """takes a float n return the floor of the float"""
+    return math.floor(n)
