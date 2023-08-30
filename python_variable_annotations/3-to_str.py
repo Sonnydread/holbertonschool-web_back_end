bcbec58c940d3ab13df89241214110810f595f41
Sonnydread <alvaradoguille590@gmail.com>
2023-08-29 17:06:55 -0700

    funct-to-str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""takes a float n returns the string of the float"""
+
+
+def to_str(n: float) -> str:
+    """takes a float n returns the string of the float"""
+    return f"{n}"
