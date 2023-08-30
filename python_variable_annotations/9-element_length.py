5eef5cb026870e06172ce3f530dc895c7fecb03b
Sonnydread <alvaradoguille590@gmail.com>
2023-08-29 18:55:28 -0700

    iterable-obj

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type an iterable object"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """type an iterable object"""
+    return [(i, len(i)) for i in lst]
