fb923b5a3b5fd08affd7e6732ac5d855080a2c89
Sonnydread <alvaradoguille590@gmail.com>
2023-09-02 09:37:46 -0700

    Async-Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""write a coroutine that takes no arguments"""
+"""a coroutine that takes no argumnets"""
 import asyncio
 from typing import List
 import random
@@ -7,6 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """write a coroutine that takes no arguments"""
-    done = [X async for X in async_generator()]
-    return result
+    """a coroutine that takes no argumnets"""
+    done = [i async for i in async_generator()]
+    return done
