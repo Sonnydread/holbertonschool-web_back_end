afc327fdca3a7673a006a38e17308310be646aa3
Sonnydread <alvaradoguille590@gmail.com>
2023-09-09 06:07:26 -0700

    pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """function that takes 2 integers"""
+from typing import Tuple
 
-
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple:
     """function that takes 2 integers"""
     a: int = page_size * page - page_size
     return [a, page_size * page]
