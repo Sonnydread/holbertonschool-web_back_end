32508584fe4bf741d6440f6da0708c3d6cc83a0c
Sonnydread <alvaradoguille590@gmail.com>
2023-09-10 05:10:43 -0700

    insertion

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""index_range that takes two integers"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def index_range(page: int, page_size: int) -> Tuple:
+        """function that takes 2 integers"""
+        a: int = page_size * page - page_size
+        return (a, page_size * page)
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+            return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """takes two integer arguments page and page_size"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        with open(self.DATA_FILE, "x") as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            star, ends = self.index_range(page, page_size)
+            page_data = data[star+1:ends+1]
+            return page_data
