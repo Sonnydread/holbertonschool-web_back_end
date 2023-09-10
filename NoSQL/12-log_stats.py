eb529ee550f7fecb65ee36a71045e12f1ce3ea5c
Sonnydread <alvaradoguille590@gmail.com>
2023-09-09 18:25:17 -0700

    Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""script that provides some stats"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost')
+    Database = client["logs"]
+    col = Database["nginx"]
+
+    print(f"{col.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for X in methods:
+        print(f"method {X}: {col.count_documents({'method': X})}")
+
+        mns = "status check"
+
+        print(f"{col.count_documents({'method': 'GET',
+                                      'path': '/status'})} {mns}")
