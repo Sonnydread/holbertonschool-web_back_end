dc5be8e63d87b09d68871898495751fedffe1db4
Sonnydread <alvaradoguille590@gmail.com>
2023-10-10 07:25:43 -0500

    combine

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter(({ location }) => location === city).map((student) => {
-    let flag = 0;
-    newGrades.forEach((grade) => {
-      if (grade.studentId === student.id) {
-        student.grade = grade.grade;
-        flag = 1;
-      }
-    });
-    if (!flag) {
-      student.grade = 'N/A';
-    }
-    return student;
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
   });
+  return cityStudents;
 }
